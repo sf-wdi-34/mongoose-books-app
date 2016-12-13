422c9a4d01f4ead41ac49964b760b4a6e5efdfde
Brianna Veenstra <bgveenstra@users.noreply.github.com>
2016-12-13 12:41:34 -0800

    mongoose promise update

--- models/index.js
+++ models/index.js
@@ -1,2 +1,7 @@
 var mongoose = require("mongoose");
+
+// use native JS promise library instead of Mongoose's deprecated one
+mongoose.Promise = global.Promise;
+
+// connect to the localhost book-app database
 mongoose.connect("mongodb://localhost/book-app");
